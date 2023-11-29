ba914b58e436fb445a3f61360f4fb6153658d28b
Kamalisri <139484778+Kamalisri07@users.noreply.github.com>
2023-11-29 16:05:33 +0530

    Add files via upload

---
+++ src/components/WeatherApp/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherApp from './WeatherApp.jsx';
+import App from './WeatherApp.jsx';
+
+
+function App(){
+  return(
+    <div className="App">
+      <WeatherApp/>
+
+    </div>
+  );
+}
+export default App;
